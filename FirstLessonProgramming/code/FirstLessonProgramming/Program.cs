c4094110a7fad6b66bbf9b2dc834716276e95de9
Nurlan Valizada <n.velizade@tayqatech.com>
2021-10-11 10:49:49 +0400

    Lesson 1 and Lesson2 added

---
+++ FirstLessonProgramming/code/FirstLessonProgramming/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+
+            int var1;
+
+            while (true)
+            {
+                try
+                {
+                    var1 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("You can only enter int number");
+                }
+            }
+
+            Console.WriteLine("Enter second number");
+            int var2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(var1, var2);
+
+            Console.WriteLine("Sum is: " + sum);
+        }
+
+
+        public static int Sum(int var1, int var2) 
+        {
+            return var1 + var2;
+        }
+    }
+}
---
+++ SecondLessonProgramming/code/SecondLessonProgramming/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SecondLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //integral numeric types
+            byte var1 = 45;
+            byte var2;
+
+            var2 = 56; //byte.Parse(Console.ReadLine());
+
+            int sum = var1 + var2;
+
+            Console.WriteLine(sum);
+
+            var intMin = int.MinValue;
+            var intMax = int.MaxValue;
+
+            //floating point numbers
+            var var3 = 567.890F;
+
+            double var4 = 45645.89D;
+
+            double sum2 = var3 + var4;
+
+            Console.WriteLine(sum2);
+
+            //boolean types
+
+            bool var5 = (6>5) || (7>5);
+
+            Console.WriteLine(var5);
+
+            // char type
+
+            char var6 = 'A';
+            char var7 = 'B';
+            var concat = var6.ToString() + var7.ToString();
+
+            Console.WriteLine(concat);
+
+            var var8 = char.IsLetter(var6);
+            var var9 = char.IsNumber(var7);
+
+            // string
+            string var10 = " asdflkndlknas ";
+            var var11 = var10.ToUpper();
+
+            Console.WriteLine(var11);
+
+            Console.ReadLine();
+        }
+    }
+}
