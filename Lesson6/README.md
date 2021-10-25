89b39b5a6080016960d617fe7915bf870cc816e8
Nurlan Valizada <n.velizade@tayqatech.com>
2021-10-25 14:53:36 +0400

    Lesson6 and it's tasks added

---
+++ Lesson6/README.md
@@ -0,0 +1,43 @@
+# Lesson6 - Tasks
+
+## Reading materials
+https://www.geeksforgeeks.org/c-sharp-methods/ \
+https://www.geeksforgeeks.org/c-sharp-method-parameters/ \
+https://www.geeksforgeeks.org/c-sharp-method-overloading/
+
+## ToDo
+Enter the value between 10 and 100. Initialize int array with random values with length of user entered value. Do following tasks
+1. Write a program in C# sort array both **descending** and **ascending** order
+2. Write a C# program to find the 3rd max element of array.
+3. Write a program in C# to find first 4 max elements.
+
+
+
+### Other tasks - Write seperate method for each task and call them in Main method
+4. Write a program in C# to create a function to swap the values of two integer numbers without additional variable.
+Test Data :
+Enter a number: 5
+Enter another number: 6
+Expected Output :
+Now the 1st number is : 6 , and the 2nd number is : 5
+
+5. Write a program in C# to create a function to display the n number Fibonacci sequence. 
+Test Data :
+Input number of Fibonacci Series : 5
+Expected Output :
+The Fibonacci series of 5 numbers is :
+0 1 1 2 3 5 8
+
+6. Write a program in C# to create a function to calculate the sum of the individual digits of a given number.
+Test Data :
+Enter a number: 1234
+Expected Output :
+The sum of the digits of the number 1234 is : 10
+
+7. Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+Test Data :
+Please input a string : This is a test string.
+Expected Output :
+"This is a test string." contains 4 spaces
+
+8. https://www.hackerrank.com/ - **Create an account** and get ready for next lesson, we are going to do some online coding next lesson
---
+++ Lesson6/code/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main()
+        {
+            (bool isOk, int result) = TryParse("436347");
+
+            int a = 5;
+
+            TryParse(Console.ReadLine(), out int var1);
+
+            TryParse("657");
+
+            IncreaseNumber(ref a);
+
+            Console.WriteLine("Inside main method " + a);
+
+
+            //method overloading
+            MultiplyNumbers(5, 6, (int)7);
+
+            MultiplyNumbers(1, 2);
+        }
+
+        static int MultiplyNumbers(double c, params int[] numbers)
+        {
+            return 0;
+        }
+
+        int MultiplyNumbers(int a, int b)
+        {
+            return a * b;
+        }
+
+        double MultiplyNumbers(double a, double b)
+        {
+            return a * b;
+        }
+
+        static int MultiplyNumbers(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+
+        static double MultiplyNumbers(int a, int b, double c)
+        {
+            return a * b * c;
+        }
+
+        static bool TryParse(string s, out int result)
+        {
+            int actualNumber;
+            try
+            {
+                actualNumber = int.Parse(s);
+                result = actualNumber;
+                return true;
+            }
+            catch (Exception e)
+            {
+                result = 0;
+                return false;
+            }
+        }
+
+        static (bool IsOk, int result) TryParse(string s)
+        {
+            try
+            {
+                var actualNumber = int.Parse(s);
+                Console.WriteLine("Process completed successfully");
+                return (true, actualNumber);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Process completed successfully");
+                return (false, 0);
+            }
+        }
+
+        static int IncreaseNumber(ref int number)
+        {
+            ++number;
+
+            Console.WriteLine("Inside method " + number);
+
+            return number;
+        }
+
+        static void Main1(string[] args)
+        {
+            SumNumbers(new[] {1, 2, 3});
+
+            SumNumbers(new[] { 1, 3 });
+
+            SumNumbers(new[] { 1, 3 , 7, 90});
+
+            SumNumbers();
+
+            SumNumbers(90);
+
+            SumNumbers(1, 2);
+
+            SumNumbers(1, 2, 6);
+
+            SumNumbers(9, 14, 67, 246, 79);
+
+            PrintHelloWorld(29, "");
+
+
+           var result = GetHelloWorldString("Nurlan", 29);
+           //Console.WriteLine(result.ToUpper());
+        }
+
+        static int SumNumbers(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+
+            return sum;
+        }
+
+        static void PrintHelloWorld(int age, string name = "Nurlan", int experience = 0)
+        {
+            var result = $"Hello World {name}, your age is {age}";
+
+            if (experience != 0)
+            {
+                result += $" and your experience is {experience}";
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static string GetHelloWorldString(string name, int age)
+        {
+            return $"Hello World {name}, your age is {age}";
+        }
+    }
+}
