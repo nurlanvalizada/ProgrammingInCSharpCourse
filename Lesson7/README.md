2e52cb69938a3f1504e830d9b21596ae665ce2e0
Nurlan Valizada <n.velizade@tayqatech.com>
2021-10-28 13:46:39 +0400

    Lesson7 added

---
+++ Lesson7/README.md
@@ -0,0 +1,9 @@
+# Lesson7 - Tasks
+
+## ToDo
+1. https://www.hackerrank.com/challenges/mini-max-sum/problem
+2. https://www.hackerrank.com/challenges/plus-minus/problem
+3. https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+4. https://www.hackerrank.com/challenges/time-conversion/submissions/code/217453861
+5. https://www.hackerrank.com/challenges/staircase/problem
+6. https://www.hackerrank.com/challenges/day-of-the-programmer/submissions
---
+++ Lesson7/code/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "This is a test string content";
+
+            int whitespaceCount = 0;
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (test[i] == ' ')
+                {
+                    whitespaceCount++;
+                }
+            }
+           
+            Console.WriteLine(whitespaceCount);
+        }
+    }
+}
