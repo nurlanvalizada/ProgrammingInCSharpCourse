2f4934695ccee0f33e306f98592134281279c1ac
Nurlan Valizada <n.velizade@tayqatech.com>
2021-11-01 18:54:08 +0400

    Lesson8 added partially

---
+++ Lesson8/README.md
@@ -0,0 +1,12 @@
+# Lesson8 - Tasks
+
+## Reading Materials
+1. https://www.w3schools.com/cs/cs_classes.php
+2. https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/tutorials/classes
+3. https://www.geeksforgeeks.org/c-sharp-class-and-object/
+4. https://www.youtube.com/watch?v=cFQLmHCguGs&ab_channel=kudvenkat
+
+## ToDo
+1. https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+2. https://www.hackerrank.com/challenges/kangaroo/problem
+3. https://www.hackerrank.com/challenges/sock-merchant/problem
---
+++ Lesson8/code/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Lesson8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.SumNumbers(5, 6);
+
+            Console.WriteLine(p.SumNumbers(5, 6));
+
+            int a = 5;
+            User.MaxUserNameLetterCount = 10;
+
+            User u = new User("Ilkin", "Shahaliyev", 2002);
+            u.PrintUserToConsole();
+
+
+            User u2 = new User();
+
+            User.MaxUserNameLetterCount = 1000;
+
+            u2.name = "Kamran";
+            u2.surname = "Shahaliyev";
+            u2.birthdate = 1994;
+            u2.PrintUserToConsole();
+
+            u.PrintUserToConsole();
+
+            User u3 = new User(2005);
+
+            User[] userArray = new User[100];
+
+            foreach (var user in userArray)
+            {
+                
+            }
+
+            u.PrintUserToConsole();
+
+            Console.WriteLine(u.GetAgeAfterXYeaars(10));
+        }
+
+        public int SumNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+
+    class User 
+    {
+        public string name;
+        public string surname;
+        public int birthdate;
+
+        public static int MaxUserNameLetterCount;
+
+        public User()
+        {
+            Program.Main(new []{""});
+            Program p = new Program();
+            p.SumNumbers(7, 8);
+        }
+
+        public User(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public User(int birthdate)
+        {
+            this.birthdate = birthdate;
+        }
+
+        public User(string name, string surname, int birthdate)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.birthdate = birthdate;
+        }
+
+        static void ChangeUserMaxLetterCount(int newCount)
+        {
+            MaxUserNameLetterCount = newCount;
+        }
+
+        public string GetFullName() 
+        {
+            return name + " " + surname;
+        }
+
+        public int GetAge() 
+        {
+            return DateTime.Now.Year - birthdate;
+        }
+
+        public void PrintUserToConsole()
+        {
+            var str = $"My name is {name} {surname}, and my birhdate is {birthdate} and my maxNameLetterCount is {MaxUserNameLetterCount}";
+            Console.WriteLine(str);
+        }
+
+        public int GetAgeAfterXYeaars(int year) 
+        {
+            return GetAge() + year;
+        }
+
+        ~User()
+        {
+
+        }
+    }
+}
