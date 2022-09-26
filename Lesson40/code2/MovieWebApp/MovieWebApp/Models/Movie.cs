11bb80a9e6d0b85142011370fdf67c419e2b7df3
Nurlan Valizada <nurlan.velizade@gmail.com>
2022-09-26 09:00:12 +0400

    MovieWebApp updated - Lesson 45,46

--- Lesson40/code2/MovieWebApp/MovieWebApp/Models/Movie.cs
+++ Lesson40/code2/MovieWebApp/MovieWebApp/Models/Movie.cs
@@ -6,11 +6,15 @@ namespace MovieWebApp.Models
     public class Movie
     {
         public int ID { get; set; }
+        
+        [StringLength(30, MinimumLength = 5)]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        
+        [Range(10, 10000, ErrorMessage = "Price is not correct")]
         public decimal Price { get; set; }
     }
 }
