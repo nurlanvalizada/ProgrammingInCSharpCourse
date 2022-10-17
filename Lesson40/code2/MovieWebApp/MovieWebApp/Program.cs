15debd80b3c4047172eb176f5dbb38fd2364521a
Nurlan Valizada <nurlan.velizade@gmail.com>
2022-10-17 16:00:40 +0400

    Css isolation lesson content committed

--- Lesson40/code2/MovieWebApp/MovieWebApp/Program.cs
+++ Lesson40/code2/MovieWebApp/MovieWebApp/Program.cs
@@ -16,7 +16,7 @@ namespace MovieWebApp
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
